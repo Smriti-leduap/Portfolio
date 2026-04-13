4330fae5e2f37685a7adcfb9788b5ecdfea773b3
Your Name <your@email.com>
2026-04-13 19:39:25 +0545

    Removal of imports

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import bgDark from '../assets/bg-dark.png';
-import bgLight from '../assets/bg-light.png';
+
 
 const Hero = () => {
   const [typedText, setTypedText] = useState('');
